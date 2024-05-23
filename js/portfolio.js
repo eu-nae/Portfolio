b0a2fb63cb8b02a7929c6e86dcd6d023d0fc13ff
eu-nae <143784684+eu-nae@users.noreply.github.com>
2024-05-23 17:41:48 +0900

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,111 @@
+$(function(){
+
+    // popup 1
+
+    $(".popup1").on("click", function(e){
+        e.preventDefault();
+        $(".popimg1").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg1").on("click",function(){
+        $(".popimg1").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    $(".popbg").on("click",function(){
+        $(".popimg1").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    // popup 2
+
+    $(".popup2").on("click", function(e){
+        e.preventDefault();
+        $(".popimg2").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg2").on("click",function(){
+        $(".popimg2").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    $(".popbg").on("click",function(){
+        $(".popimg2").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    // popup 3
+
+    $(".popup3").on("click", function(e){
+        e.preventDefault();
+        $(".popimg3").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg3").on("click",function(){
+        $(".popimg3").fadeOut();
+        $(".popbg").fadeOut();
+    });
+    
+    $(".popbg").on("click",function(){
+        $(".popimg3").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    // popup 4
+
+    $(".popup4").on("click", function(e){
+        e.preventDefault();
+        $(".popimg4").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg4").on("click",function(){
+        $(".popimg4").fadeOut();
+        $(".popbg").fadeOut();
+    });
+    
+    $(".popbg").on("click",function(){
+        $(".popimg4").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    // popup 5
+
+    $(".popup5").on("click", function(e){
+        e.preventDefault();
+        $(".popimg5").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg5").on("click",function(){
+        $(".popimg5").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    $(".popbg").on("click",function(){
+        $(".popimg5").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    // popup 6
+
+    $(".popup6").on("click", function(e){
+        e.preventDefault();
+        $(".popimg6").fadeIn();
+        $(".popbg").fadeIn();
+    });
+
+    $(".popimg6").on("click",function(){
+        $(".popimg6").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+    $(".popbg").on("click",function(){
+        $(".popimg6").fadeOut();
+        $(".popbg").fadeOut();
+    });
+
+});
